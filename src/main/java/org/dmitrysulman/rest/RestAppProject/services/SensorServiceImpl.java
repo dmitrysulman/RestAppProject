74b3ee25ac1f029c020c878a461dbfb70928596f
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-10-14 12:48:45 +0300

    c1

--- src/main/java/org/dmitrysulman/rest/RestAppProject/services/SensorServiceImpl.java
+++ src/main/java/org/dmitrysulman/rest/RestAppProject/services/SensorServiceImpl.java
@@ -3,10 +3,12 @@ package org.dmitrysulman.rest.RestAppProject.services;
 import org.dmitrysulman.rest.RestAppProject.models.Sensor;
 import org.dmitrysulman.rest.RestAppProject.repositories.SensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Service
 @Transactional(readOnly = true)
 public class SensorServiceImpl implements SensorService {
     private final SensorRepository sensorRepository;
